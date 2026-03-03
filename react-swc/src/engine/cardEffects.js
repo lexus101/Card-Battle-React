1e4edef3deacd1368ff8872557dc96fd48494eaa
lexus101 <89865282+lexus101@users.noreply.github.com>
2026-03-02 23:15:32 -0800

    Card Image

--- react-swc/src/engine/cardEffects.js
+++ react-swc/src/engine/cardEffects.js
@@ -4,6 +4,7 @@ export const CardLibrary = {
     id: 'STRIKE',
     name: "Strike",
     description:"Deal 5 dmg to target.",
+    image: "/Strike.png",
     time_cost: 1,
     effects: [
       { type: 'DAMAGE', value: 5, target: 'target' }
@@ -13,6 +14,7 @@ export const CardLibrary = {
     id: 'DEFEND',
     name: "Defend",
     description:"Gain 8 Shield.",
+    image: "/Defend.png",
     time_cost: 1,
     effects: [
       { type: 'SHIELD', value: 8, target: 'target' }
@@ -22,6 +24,7 @@ export const CardLibrary = {
     id: 'PATCH_UP',
     name: "Patch Up",
     description:"Heal 4 hp for target.",
+    image: "/Patch Up.png",
     time_cost: 1,
     effects: [
       { type: 'HEAL', value: 4, target: 'target' }
@@ -31,6 +34,7 @@ export const CardLibrary = {
     id: 'QUICK_STEP',
     name: "Quick Step",
     description:"Gain 3 Shield. Then Draw 1.",
+    image: "/Quick Step.png",
     time_cost: 0,
     effects: [
       { type: 'SHIELD', value: 3, target: 'target' },
@@ -41,6 +45,7 @@ export const CardLibrary = {
     id: 'FOCUS',
     name: "Focus",
     description:"Draw 2.",
+    image: "/Focus.png",
     time_cost: 0,
     effects: [
       { type: 'DRAW', value: 2, target: 'self' }
